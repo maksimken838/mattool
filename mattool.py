6a3c658934e918c93893fd3fbbefe1be9aff1d6a
maksimken838 <maksimken838@gmail.com>
2019-10-20 12:56:09 +0300

    fix the input please

--- mattool.py
+++ mattool.py
@@ -28,15 +28,14 @@ class Matrix:
             return False
 
     def augment(self):
-        print('debug message')
         if self.isSquare():
             for i in range(self.strings):
                 for j in range(self.rows):
-                    if self.strings == self.rows:
+                    if i == j:
                         self.matrix[i].append(1)
                     else:
                         self.matrix[i].append(0)
-            self.rows == self.rows * 2
+            self.rows = self.rows * 2
             self.isAugmented = True
         else:
             print('Error: trying to add identity matrix to matrix which is not square\n')
@@ -48,13 +47,11 @@ class Matrix:
             for j in range(self.rows):
                 if j == 0:
                     print('(', end = ' ')
-                elif self.isAugmented and j == self.rows:
-                    print('|', end = '  ')
+                elif self.isAugmented and j == self.strings:
+                    print('|', end = ' ')
+                print(str(self.matrix[i][j]).rjust(self.maxlength), end = ' ')
                 if j == self.rows - 1:
-                    print(str(self.matrix[i][j]).rjust(self.maxlength) + ' )', end = '\n')
-                else:
-                    print(str(self.matrix[i][j]).rjust(self.maxlength), end = '  ')
-
+                    print(')', end = '\n')
         print('\nYou still can use commands\n')
 
 "The end of Matrix class"
@@ -80,32 +77,21 @@ def console():
     if command == 'input':
         print('\nNow you should define size of the matrix - type two numbers, first for number of columns and second for number of rows\n')
         n, m = [int(x) for x in input().split()]
-        print()
         print('\nNow just type your matrix line by line\n')
-        print()
         A = []
         for i in range(n):
             A.append(list(input().split()))
         X = Matrix(n, m, A)
-        X.prt()
     elif command == 'add':
         print('\nNow type 3 numbers (What string, to what string, with what multiplier)\n')
         a, b, c = [float(x) for x in input().split()]
         X.add(int(a) - 1, int(b) - 1, c)
-        X.prt()
-    elif command == 'fuck':
-        print("debug message_0")
+    elif command == 'augment':
         X.augment()
-        X.prt()
-        X.augment()
-        print(X)
-    elif command == 'clear':
-        X.prt()
     elif command == 'exit':
         return
     else:
         print('Command does not exist')
-        system(read)
     console()
 
 console()
